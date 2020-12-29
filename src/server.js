46d6400b88483b3091e57af9d3f0b1c7d6c86c71
igstbagusdharmaputra <dharmatkjone@gmail.com>
2020-12-30 05:09:25 +0800

    update

--- src/server.js
+++ src/server.js
@@ -9,5 +9,5 @@ const express = require('express');
    res.send('Hello World\n');
  });
  app.listen(PORT, ()=> {
-	console.log(`Server running port ${PORT}GAS`);
+	console.log(`Server running port ${PORT}HAI`);
  });
